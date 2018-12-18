1c1c9248847fb1e669c24fc66ee05039adaaff60
jonathanware <jonathan.ware@gmail.com>
2018-12-18 07:23:08 -0700

    added variables and stuff

--- myScript.js
+++ myScript.js
@@ -3,3 +3,13 @@
 function myFunction () {
     document.getElementById('demo').innerHTML = "Paragraph Changed";
 }
+
+//defining variables
+var x = 5;
+var y = 6;
+var z = x + y;
+
+//Injecting the math into the html
+function mathFunction () {
+    document.getElementById('math').innerHTML = z;
+}
